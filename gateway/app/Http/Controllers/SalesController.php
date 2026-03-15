1769186858506245152fe1eeea670f01a72bd49a
davidparra75 <daparraca@unal.edu.co>
2026-03-15 18:39:19 -0500

    Feat: create salescontrolers

--- gateway/app/Http/Controllers/SalesController.php
+++ gateway/app/Http/Controllers/SalesController.php
@@ -8,6 +8,34 @@ use Illuminate\Support\Facades\Http;
 class SalesController extends Controller
 {
     public function registerSale(Request $request){
+       $productId=$request->product_id;
+       $quantity=$request->quantity;
+
+       // Consultar inventario en flask
+       $inventory = Http::get("http://localhost:5000/products/$productId");
+
+       if (!$inventory -> succesful()){
+        return response()->json(['error'=>'Error consultando inventario'],500);
+       }
        
+       $stock = $inventory>json()['stock'];
+
+       // 2 verifica stock
+       if ($stock < $quantity){
+        return response()->json(['error'=>'Cantidad en stock insuficiente'],400);
+       }
+       
+       //3 venta en express
+       $sale = Http::post("http://localhost:3000/sales",[
+           'product_id'=>$productId,
+           'quantity'=>$quantity,
+           'user_id'=>auth()->user()->id
+        ]);
+
+        //4 actualizar inventario flask
+        Http::put("http://localhost:5000/products/$productId",[
+            'quantity'=>$quantity
+        ]);
+        return response()->json($sale>json());
     }
 }
